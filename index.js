6e037144392a8a73b24c000aa22bb4cf3923ebbb
“[Montel <[mhsports345@gmail.com]>
2024-01-08 17:55:57 -0500

    Revised HTML page, built 2 functions

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 const myLibrary = [];
 
-function Book() {
-  // the constructor...
+function Book(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
 }
 
 function addBookToLibrary() {
-  // do stuff here
+  const title = prompt("Enter the book title:");
+  const author = prompt("Enter the author's name:");
+  const pages = prompt("Enter the number of pages:");
+  const read = prompt("Has the book been read?:");    // did prompt rather than confirm to enter value
+  const newBook = new Book(title, author, pages, read);  // new Book object
+
+  myLibrary.push(newBook); // Add the new book to the library. 
+
+  console.log("Update Library:", myLibrary);
 }
+
+addBookToLibrary()
